abb6d60fa739cca8b008c56f3b5c39c5c8f1db57
marc2332 <mespinsanz@gmail.com>
2023-07-08 23:35:08 +0200

    improvements

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,15 @@ struct AppProps {
 
 fn app(cx: Scope<AppProps>) -> Element {
     render!(
+        head {
+            title {
+                "{cx.props.user} | ghboard"
+            }
+            link {
+                rel: "icon",
+                href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦑</text></svg>"
+            }
+        }
         script {
             src: "https://cdn.tailwindcss.com"
         }
